7feb3d83fcd1658056a0748626a0c68f88469ac4
danielnoo <dannynoo@gmail.com>
2021-10-17 18:18:04 -0400

    added copy for all portfolio pieces, made a javascript function to attach event listeners to project boxes

--- scripts.js
+++ scripts.js
@@ -2,21 +2,43 @@
 
 const app = {}
 
-app.projOne = document.querySelector('#projectOneParent');
-app.projOneDetails = document.querySelector('#projectOneDetails')
+app.projectParents = []
+app.projectDetails = []
+
+app.projectParents[0] = document.querySelector('#projectOneParent');
+app.projectDetails[0] = document.querySelector('#projectOneDetails');
+app.projectParents[1] = document.querySelector('#projectTwoParent');
+app.projectDetails[1] = document.querySelector('#projectTwoDetails');
+app.projectParents[2] = document.querySelector('#projectThreeParent');
+app.projectDetails[2] = document.querySelector('#projectThreeDetails');
+app.projectParents[3] = document.querySelector('#projectFourParent');
+app.projectDetails[3] = document.querySelector('#projectFourDetails');
+app.projectParents[4] = document.querySelector('#projectFiveParent');
+app.projectDetails[4] = document.querySelector('#projectFiveDetails');
+app.projectParents[5] = document.querySelector('#projectSixParent');
+app.projectDetails[5] = document.querySelector('#projectSixDetails');
 
 
 
 
 app.init = () => {
 
-  app.projOne.addEventListener('mouseover', () => {
-    app.projOneDetails.classList.add('mouseOverSlide')
-    console.log('mousing over');
+  // loop over the array of projects and add event listeners to 
+  // apply and remove the slide-over information div on hover
+
+  app.projectParents.forEach((project, index) => {
+    project.addEventListener('mouseover', () => {
+      app.projectDetails[index].classList.add('mouseOverSlide');
+    })
+    project.addEventListener('mouseout', () => {
+      app.projectDetails[index].classList.remove('mouseOverSlide');
+    })
   })
-  app.projOne.addEventListener('mouseout', () => {
-    app.projOneDetails.classList.remove('mouseOverSlide')
-  })
+
+
+  
+  
+  
 }
 
 app.init();
