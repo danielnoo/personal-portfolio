49a47581e59c715bc4cf9ec48542c88000905240
danielnoo <dannynoo@gmail.com>
2021-10-18 00:16:33 -0400

    completed project type selection

--- scripts.js
+++ scripts.js
@@ -2,8 +2,11 @@
 
 const app = {}
 
-app.projectParents = []
-app.projectDetails = []
+
+// declarations for buttons and hover effects
+
+app.projectParents = [];
+app.projectDetails = [];
 
 app.projectParents[0] = document.querySelector('#projectOneParent');
 app.projectDetails[0] = document.querySelector('#projectOneDetails');
@@ -17,6 +20,47 @@ app.projectParents[4] = document.querySelector('#projectFiveParent');
 app.projectDetails[4] = document.querySelector('#projectFiveDetails');
 app.projectParents[5] = document.querySelector('#projectSixParent');
 app.projectDetails[5] = document.querySelector('#projectSixDetails');
+
+
+
+app.allButton = document.querySelector('#allButton');
+app.reactButton = document.querySelector('#reactButton');
+app.jsButton = document.querySelector('#jsButton');
+
+
+
+app.selectProjectStyle = (e) => {
+  app.projectParents.forEach(project => {
+    project.classList.remove('notVisible');
+  })
+  if(e.target.innerText === 'All') {
+    app.projectParents.forEach(project => {
+      if(project.classList.contains('notVisible')) {
+         project.classList.toggle('notVisible');
+      } 
+     
+    })
+  } else if(e.target.innerText === 'React') {
+    app.projectParents.forEach((project, index) => {
+      if(index > 1) {
+        project.classList.toggle('notVisible');
+      }
+    })
+  } else {
+    app.projectParents.forEach((project, index) => {
+      if(index === 2 || index === 5) {
+        project.classList.remove('notVisible');
+      } else {
+        project.classList.add('notVisible');
+      }
+    })
+  }
+}
+
+
+
+
+
 
 
 
@@ -35,10 +79,10 @@ app.init = () => {
     })
   })
 
+  allButton.addEventListener('click', app.selectProjectStyle);
+  reactButton.addEventListener('click', app.selectProjectStyle);
+  jsButton.addEventListener('click', app.selectProjectStyle);
 
-  
-  
-  
 }
 
 app.init();
